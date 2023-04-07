975ff019ddc53bc06e54293395ca2cc8b4a71d86
Author Name <hello@bobrovartem.ru>
2023-04-07 19:39:43 +0300

    Add header

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./app-header.module.css";
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+export default class AppHeader extends React.Component {
+  render() {
+    return (
+      <header className={styles.header}>
+        <div className={styles.container}>
+          <nav>
+            <ul className={styles.menu}>
+              <li className={styles.menuItem}>
+                <BurgerIcon type="primary" />
+                <span className={styles.menuItemText}>Конструктор</span>
+              </li>
+              <li className={styles.menuItem}>
+                <ListIcon type="secondary" />
+                <span className={styles.menuItemText}>Лента заказов</span>
+              </li>
+            </ul>
+          </nav>
+          <Logo />
+          <nav>
+            <ul className={styles.rightMenu}>
+              <li className={styles.menuItem}>
+                <ProfileIcon type="secondary" />
+                <span className={styles.menuItemText}>Личный кабинет</span>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppHeader from "../app-header/app-header";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <AppHeader />
+      </>
+    );
+  }
+}
