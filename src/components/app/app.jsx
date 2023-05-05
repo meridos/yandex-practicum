5aedb4125bf77edd1934eb39c073d902a3868a7e
Artem Bobrov <hello@bobrovartem.ru>
2023-05-06 00:42:05 +0300

    add router & home page

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getIngredients } from "../../services/actions/ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
+
+import { RouterProvider } from "react-router-dom";
+import AppHeader from "../app-header/app-header";
 import ErrorBoundary from "../error-boundary/error-boundary";
 import styles from "./app.module.css";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { DndProvider } from "react-dnd";
-import AppHeader from "../app-header/app-header";
+import { router } from "./app-router";
 
 export default function App() {
   const dispatch = useDispatch();
@@ -21,19 +20,7 @@ export default function App() {
     <ErrorBoundary error={overlayError}>
       <div className={styles.wrapper}>
         <AppHeader />
-        <DndProvider backend={HTML5Backend}>
-          <main className={styles.container}>
-            <div className={styles.side}>
-              <h1 className="text text_type_main-large mt-10 mb-5">
-                Соберите бургер
-              </h1>
-              <BurgerIngredients />
-            </div>
-            <div className={styles.constructorContainer}>
-              <BurgerConstructor />
-            </div>
-          </main>
-        </DndProvider>
+        <RouterProvider router={router} />
       </div>
     </ErrorBoundary>
   );
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,23 @@
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import BurgerConstructor from "../../components/burger-constructor/burger-constructor";
+import BurgerIngredients from "../../components/burger-ingredients/burger-ingredients";
+import styles from "./home.module.css";
+
+export function Home() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <main className={styles.container}>
+        <div className={styles.side}>
+          <h1 className="text text_type_main-large mt-10 mb-5">
+            Соберите бургер
+          </h1>
+          <BurgerIngredients />
+        </div>
+        <div className={styles.constructorContainer}>
+          <BurgerConstructor />
+        </div>
+      </main>
+    </DndProvider>
+  );
+}
