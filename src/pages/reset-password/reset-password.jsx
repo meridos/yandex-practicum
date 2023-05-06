a25780db99de10470211eba5d341df25a38359e8
Artem Bobrov <hello@bobrovartem.ru>
2023-05-06 22:04:55 +0300

    верстка страницы подтверждения восстановления пароля

--- src/pages/reset-password/reset-password.jsx
+++ src/pages/reset-password/reset-password.jsx
@@ -1,5 +1,100 @@
+import {
+  Button,
+  Input,
+  PasswordInput,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "./reset-password.module.css";
 
+const PASSWORD_ERROR = "Некорректный пароль";
+const CODE_ERROR = "Некорректный код";
+
 export function ResetPasswordPage() {
-  return <h1>ResetPassword</h1>;
+  const [password, setPassword] = useState("");
+  const passwordTyped = useRef(false);
+  const [passwordError, setPasswordError] = useState(PASSWORD_ERROR);
+
+  const [code, setCode] = useState("");
+  const [codeError, setCodeError] = useState(CODE_ERROR);
+  const codeTyped = useRef(false);
+
+  const [formValid, setFormValid] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onCodeBlur = (e) => {
+    setCodeError(code ? "" : CODE_ERROR);
+  };
+
+  useEffect(() => {
+    setFormValid(!codeError && !passwordError);
+  }, [codeError, passwordError]);
+
+  useEffect(() => {
+    if (code && !codeTyped.current) {
+      codeTyped.current = true;
+    }
+  }, [code]);
+
+  useEffect(() => {
+    if (password && !passwordTyped.current) {
+      passwordTyped.current = true;
+    }
+
+    if (!password && passwordTyped.current) {
+      setPasswordError(PASSWORD_ERROR);
+    }
+
+    if (password) {
+      setPasswordError("");
+    }
+  }, [password]);
+
+  return (
+    <form onSubmit={onSubmit} className={styles.container}>
+      <h2 className="text text_type_main-medium">Восстановление пароля</h2>
+      <PasswordInput
+        placeholder="Введите новый пароль"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        name="password"
+        error={!!passwordError && passwordTyped.current}
+        errorText={passwordError}
+      />
+      <Input
+        type="text"
+        placeholder="Введите код из письма"
+        value={code}
+        name="code"
+        error={!!codeError && codeTyped.current}
+        errorText={codeError}
+        noValidate={true}
+        onChange={(e) => setCode(e.target.value)}
+        onBlur={onCodeBlur}
+        onFocus={() => setCodeError()}
+      />
+      <Button
+        htmlType="submit"
+        type="primary"
+        size="medium"
+        disabled={!formValid}
+      >
+        Восстановить
+      </Button>
+
+      <div className={styles.links}>
+        <div className="mb-4">
+          <span className="mr-2 text text_type_main-default text_color_inactive">
+            Вспомнили пароль?
+          </span>
+          <Link to="/login" className={styles.link}>
+            Войти
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
 }
